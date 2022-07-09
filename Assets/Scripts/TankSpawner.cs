e49c171511b648420cf210ea9b506eef75827bc0
aadya1710 <aadya.mittal17@gmail.com>
2022-07-09 20:33:57 +0530

    First Commit

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tankprefab, transform.position, Quaternion.identity);//quaternion is used to avoid any rotation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
